ec54f51e778acd69e8acbd5251fcdabd2803a5e1
Mark Laane <marklaane@gmail.com>
2014-07-07 16:47:03 +0300

    Added state-machine *fixed many small bugs

--- breaker.py
+++ breaker.py
@@ -3,6 +3,7 @@ __author__ = 'Mark'
 import math
 import random
 from threading import Thread
+from enum import Enum
 
 # "Cairocffi" could be also installed as "cairo"
 try:
@@ -17,31 +18,106 @@ from game_elements_library import Player, Paddle, Ball, Brick, delayed_function_
 
 
 class Breaker(game.Game):
+    class State(Enum):
+        starting_delay = 0
+        waiting_push = 1
+        running = 2
+        finished = 3
+
     def __init__(self, field_dims):
         self.field_dims = field_dims
+
         self.player = Player()
         self.paddle = Paddle(0, self.field_dims[1] - 4)  # Paddle on the bottom
         self.balls = []
         self.bricks = []
-        self.reset_game()
+        self._state = None
+        self.ball_speed = None  # Speed set for all current balls
 
-    def reset_game(self):
-        self.reset_bricks()
+        self._reset_game()
+
+    def button_pressed(self, button_number):
+        """
+        Signal the game that the button was pressed.
+
+        :param button_number: number of the board-button pressed.
+        """
+
+        if not(0 <= button_number <= 9):
+            raise ValueError("Invalid ButtonNumber")
+
+        #Start the game if we were waiting for user
+        if self._state == Breaker.State.waiting_push:
+            self._state = Breaker.State.running
+
+        self.paddle.set_target_position(10 / 2 + 10 * button_number)
+
+    def step(self):
+        if self._state != Breaker.State.running:
+            return
+
+        #Make game quicker
+        self.paddle.speed += 0.001
+        self.ball_speed += 0.001
+        for ball in self.balls:
+            ball.speed = self.ball_speed
+
+        self.paddle.step()
+        self.paddle.limit(self.field_dims[1] - 1)
+
+        for ball in self.balls:
+            ball.step()
+            self._test_ball_collisions(ball)
+
+            if self._is_ball_outside(ball):
+                self.balls.remove(ball)
+
+            if len(self.balls) == 0:
+                self.player.lose_hp()
+                self.paddle.set_health(self.player.hp, self.player.max_hp)
+                if self.player.state == Player.State.alive:
+                    Thread(target=delayed_function_call, args=(1, self._new_ball)).start()
+                else:
+                    self._state = Breaker.State.finished
+                    Thread(target=delayed_function_call, args=(5, self._reset_game)).start()
+
+        if self._state == Breaker.State.running and self._all_bricks_broken():
+            self._state = Breaker.State.finished
+            Thread(target=delayed_function_call, args=(1, self._reset_game)).start()
+
+    def draw(self, ctx):
+        for ball in self.balls:
+            ball.draw(ctx)
+
+        self.paddle.draw(ctx)
+        for brick in self.bricks:
+            brick.draw(ctx)
+
+    def _reset_game(self):
+        self._state = Breaker.State.starting_delay
+        self.ball_speed = 1
+        self.paddle.speed = 1
+        self._reset_bricks()
+        self.balls = []
         self.paddle.set_position((self.field_dims[0] - 1) / 2)
-        self.paddle.set_health(1)
+        self.paddle.set_health(self.player.max_hp, self.player.max_hp)
         self.player.reset()
-        self.new_ball()
+        Thread(target=delayed_function_call, args=(2, self._start_waiting)).start()
 
-    def test_ball_collisions(self, ball):
+    def _start_waiting(self):
+        self._new_ball()
+        self._state = Breaker.State.waiting_push
+
+    def _test_ball_collisions(self, ball):
         collide_ball_to_paddle(ball, self.paddle)
         collide_to_left_wall(ball)
         collide_to_right_wall(ball, self.field_dims[0])
         collide_to_top_wall(ball)
         for brick in self.bricks:
             if not brick.broken:
-                self.collide_ball_to_brick(ball, brick)
+                self._collide_ball_to_brick(ball, brick)
 
-    def collide_ball_to_brick(self, ball, brick):
+    def _collide_ball_to_brick(self, ball, brick):
         if are_colliding_rect_rect(ball, brick):
             intersection = brick.intersection(ball)
             if intersection.width > intersection.height:
@@ -60,19 +136,22 @@ class Breaker(game.Game):
             brick.broken = True
             probability = 0.1
             if random.random() < probability:
-                self.new_ball(ball.speed)  # add a ball with same speed
+                self._new_ball()  # add a ball with same speed
 
-    def is_ball_outside(self, ball):
+    def _is_ball_outside(self, ball):
         pixels_out = 1
         if ball.top > (self.field_dims[1] - 1) + pixels_out:
             return True
         return False
 
-    def new_ball(self, speed=1):
+    def _new_ball(self):
         heading = math.pi / 2 + 0.15 * (random.randint(0, 1) * 2 - 1)
-        self.balls.append(Ball(self.paddle.center_x, self.paddle.center_y, speed, heading))
+        ball = Ball(self.paddle.center_x, self.paddle.top, self.ball_speed, heading)
+        ball.center_y -= ball.radius
+        self.balls.append(ball)
 
-    def reset_bricks(self):
+    def _reset_bricks(self):
+        distance_from_top = 15
         dims = 6, 4
         brick_dims = 14, 6
         patterns = cairo.SolidPattern(0, 0, 255), cairo.SolidPattern(0, 255, 255)
@@ -80,47 +159,15 @@ class Breaker(game.Game):
             for x in range(dims[0]):
                 self.bricks.append(
                     Brick(
-                        (self.field_dims[0] - brick_dims[0] * dims[0]) / 2 + x * brick_dims[0],
-                        5 + y * brick_dims[1],
-                        brick_dims[0],
-                        brick_dims[1],
-                        patterns[(x + y) % 2]
+                        x=(self.field_dims[0] - brick_dims[0] * dims[0]) / 2 + x * brick_dims[0],
+                        y=distance_from_top + y * brick_dims[1],
+                        width=brick_dims[0],
+                        height=brick_dims[1],
+                        pattern=patterns[(x + y) % 2]
                     )
                 )
 
-    def step(self):
-        self.paddle.step()
-        self.paddle.limit(self.field_dims[1] - 1)
-
-        for ball in self.balls:
-            ball.step()
-            self.test_ball_collisions(ball)
-
-            if self.is_ball_outside(ball):
-                self.balls.remove(ball)
-
-            if len(self.balls) == 0:
-                self.player.lose_hp()
-                self.paddle.set_health(self.player.hp / self.player.max_hp)
-                if self.player.state == Player.State.alive:
-                    Thread(target=delayed_function_call, args=(1, self.new_ball)).start()
-                else:
-                    Thread(target=delayed_function_call, args=(5, self.reset_game)).start()
-
-        if self.all_bricks_broken():
-            pass
-            #TODO:
-            #Thread(target=delayed_function_call, args=(1, self.reset_game)).start()
-
-    def draw(self, ctx):
-        for ball in self.balls:
-            ball.draw(ctx)
-
-        self.paddle.draw(ctx)
-        for brick in self.bricks:
-            brick.draw(ctx)
-
-    def all_bricks_broken(self):
+    def _all_bricks_broken(self):
         for brick in self.bricks:
             if not brick.broken:
                 return False
