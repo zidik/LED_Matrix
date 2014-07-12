c81b7e3a1fe6a017b0d838a82c63db71da0657de
Mark Laane <marklaane@gmail.com>
2014-07-12 15:07:09 +0300

    Code reformat and small fix

--- ajax.js
+++ ajax.js
@@ -1,23 +1,23 @@
-window.onload=function(){
+window.onload = function () {
     document.getElementById('mode_submit').style.visibility = 'hidden';
 };
 
-function ajaxPOST(target_page, content_type, values){
+function ajaxPOST(target_page, content_type, values) {
     var xmlhttp;
-    if (window.XMLHttpRequest){
+    if (window.XMLHttpRequest) {
         // code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp=new XMLHttpRequest();
+        xmlhttp = new XMLHttpRequest();
     }
-    else{
+    else {
         // code for IE6, IE5
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    xmlhttp.onreadystatechange=function(){
-            if (xmlhttp.readyState==4 && xmlhttp.status==200){
-                document.getElementById("server_response").innerHTML=xmlhttp.responseText;
-            }
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            document.getElementById("server_response").innerHTML = xmlhttp.responseText;
         }
-    xmlhttp.open("POST",target_page,true);
+    };
+    xmlhttp.open("POST", target_page, true);
     xmlhttp.setRequestHeader("Content-type", content_type);
     xmlhttp.send(values);
 }
